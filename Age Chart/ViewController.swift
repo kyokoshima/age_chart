11b583a1e42b5ab0e1eab0b8cdfa2a0a3b5497af
kyokoshima <dev.kyoko@gmail.com>
2017-11-21 06:57:24 +0700

    Initial commit

--- Age Chart/ViewController.swift
+++ Age Chart/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ages: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    var dataSource = [[String: String]]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        addDoneButtonOnKeyboard()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func didTextValueChanged(_ sender: Any) {
+        var source = [[String: String]]()
+        if let textField = sender as? UITextField {
+            let age = textField.text as! String
+            source.append(["1": age])
+            source.append(["2": age])
+            source.append(["3": age])
+            dataSource = source
+            tableView.reloadData()
+        }
+        
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x:0, y:0, width:320, height:50))
+        doneToolbar.barStyle = UIBarStyle.blackTranslucent
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem =
+            UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(ViewController.doneButtonAction))
+        
+        var items = [UIBarButtonItem]()
+        items.append(flexSpace)
+        items.append(done)
+        
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.ages.inputAccessoryView = doneToolbar
+        
+    }
+    
+    @objc func doneButtonAction()
+    {
+        self.ages.resignFirstResponder()
+    }
+    
 }
 
+extension ViewController : UITableViewDelegate {
+    
+}
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "agesCell", for: indexPath)
+        //ここで先ほど指定した『beginnerCell』を呼んでる。
+        let text = dataSource[indexPath.row].first?.value as! String
+        cell.textLabel?.text = text
+        
+        return cell
+        
+    }
+}
